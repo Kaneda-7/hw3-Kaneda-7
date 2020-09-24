3021d3f6abfb93b045a16ebd0ee7114917a197e0
mikiehan <mhan@calbaptist.edu>
2020-09-23 22:17:46 -0700

    Initial commit

---
+++ src/BabyNames.java
@@ -0,0 +1,31 @@
+public class BabyNames {
+
+    //class constants for open area in graphics (Do NOT change)
+    private static final Integer OPEN_AREA_WIDTH = 780;
+    private static final Integer OPEN_AREA_HEIGHT = 500;
+
+    //prompt msg class constants (Do NOT change)
+    private static final String MESSAGE_PREFIX = "This program allows you to search through the\n" +
+            "data from the Social Security Administration\n" +
+            "to see how popular a particular name has been\n" +
+            "since ";
+
+    // class constant for meaning file (Do NOT change)
+    private static final String MEANING_FILENAME = "meanings.txt";
+
+    // class constant for name file (Change the value only. Do NOT change the names of the constant)
+    // Test with both "names.txt" and "names2.txt" (Before submission, change back to "names.txt")
+    private static final String NAME_FILENAME = "names.txt";
+
+    // Other class constants (Change the value only. Do NOT change the names of the constants)
+    private static final Integer STARTING_YEAR = 0; // change the value according to spec
+    private static final Integer DECADE_WIDTH = 0; // change the value according to spec
+    private static final Integer LEGEND_HEIGHT = 0; // change the value according to spec
+
+    // YOU ARE NOT ALLOWED TO ADD ANY OTHER CONSTANTS THAN ABOVE
+
+    public static void main(String[] args) {
+        //Remove this comment and below line and implement your code
+        System.out.println("Import Success! Now delete this code and implement your own!");
+    }
+}
